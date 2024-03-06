4f943b9da4df3f8d613b47847dcbd3fd91d50a59
Shervin Tajdini <shervin.taj@gmail.com>
2024-03-06 12:25:44 -0500

    check horizontal

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,30 @@ function App() {
 const [board,setBoard] = useState(generateBoard(3));
 const[curPlayer, setCurPlayer] = useState('X');
 
+const checkHorizontal = (board) => {
+  for(let row of board){
+    const rowSet = new Set(row);
+    if(rowSet.size === 1 && !rowSet.has(undefined)){
+      return true;
+    }
+  }
+}
+const checkForWin = (board) => {
+  //horizontal
+  if(checkHorizontal(board)){
+    return true;
+  }
+  //vertical
+
+  //diagonal
+
+}
 const handleClick = (r,c) => {
   board[r][c] = curPlayer;
   setBoard([...board]);
+  if(checkForWin(board)){
+    console.log(curPlayer + ' wins');
+  }
   setCurPlayer(curPlayer === 'X'? 'Y':'X');
 }
   return (
